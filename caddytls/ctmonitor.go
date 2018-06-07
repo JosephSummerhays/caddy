d54c12064d1abc4d6c59b38abcd15458db151267
Joseph Summerhays <summerhays.joseph@gmail.com>
2018-06-07 12:51:06 -0600

    ctmonitor no longer empty

--- caddytls/ctmonitor.go
+++ caddytls/ctmonitor.go
@@ -1,1 +1,25 @@
 package caddytls
+
+import (
+	"fmt"
+
+	"github.com/mholt/caddy"
+)
+
+func getMonitoredCerts() (monitoredCerts []string) {
+	for _, inst := range caddy.Instances() {
+		inst.StorageMu.RLock()
+		certCache, ok := inst.Storage[CertCacheInstStorageKey].(*certificateCache)
+		inst.StorageMu.RUnlock()
+		if !ok || certCache == nil {
+			continue
+		}
+		certCache.RLock()
+		for _, cert := range certCache.cache {
+			fmt.Println(cert.Names)
+			monitoredCerts = append(monitoredCerts, cert.Names...)
+		}
+		certCache.RUnlock()
+	}
+	return
+}
